3ec5d775a205edf6257135efbe090db71ca66413
kimchan122 <chanchankim122@gmail.com>
2022-08-11 01:38:48 +0900

    kc_er

--- dapp/src/components/Main/index.js
+++ dapp/src/components/Main/index.js
@@ -73,7 +73,6 @@ const Main = () => {
         <S.Container>
             <Card>
                 <Card.Body className='App'>
-<<<<<<< HEAD
                     <Row xs="3">
                         <Col><ListGroup horizontal>
                             <ListGroup.Item><S.Account1>Account</S.Account1></ListGroup.Item>
@@ -96,67 +95,26 @@ const Main = () => {
                                         <ListGroup.Item><S.SBT1>FileCoindddddfasdfasdfasdf</S.SBT1></ListGroup.Item>
                                         <ListGroup.Item><S.SBT2>blah blah no example</S.SBT2></ListGroup.Item>
                                         <ListGroup.Item><S.SBT3><Badge bg="secondary">10</Badge></S.SBT3></ListGroup.Item>
-                                        <ListGroup.Item><S.SBT4><Button className='b' variant="outline-secondary" onClick={() => printMsg("To be implemented...")}>View</Button></S.SBT4></ListGroup.Item>
+                                        {/* <ListGroup.Item><S.SBT4><Button className='b' variant="outline-secondary" onClick={() => printMsg("To be implemented...")}>View</Button></S.SBT4></ListGroup.Item> */}
                                     </ListGroup>
                                     <br />
                                     <ListGroup horizontal='sm' className='d'>
                                         <ListGroup.Item><S.SBT1>ETH</S.SBT1></ListGroup.Item>
                                         <ListGroup.Item><S.SBT2>blah blah 'SM' example</S.SBT2></ListGroup.Item>
                                         <ListGroup.Item><S.SBT3><Badge bg="secondary">30</Badge></S.SBT3></ListGroup.Item>
-                                        <ListGroup.Item><S.SBT4><Button className='b' variant="outline-secondary" onClick={() => handleShow('md-down')}>View</Button></S.SBT4></ListGroup.Item>
+                                        {/* <ListGroup.Item><S.SBT4><Button className='b' variant="outline-secondary" onClick={() => handleShow('md-down')}>View</Button></S.SBT4></ListGroup.Item> */}
                                     </ListGroup>
                                     <br />
                                     <ListGroup horizontal='md' className='d'>
                                         <ListGroup.Item><S.SBT1>Polygon</S.SBT1></ListGroup.Item>
                                         <ListGroup.Item><S.SBT2>blah blah 'MD' example</S.SBT2></ListGroup.Item>
                                         <ListGroup.Item><S.SBT3><Badge bg="secondary">50</Badge></S.SBT3></ListGroup.Item>
-                                        <ListGroup.Item><S.SBT4><Button className='b' variant="outline-secondary" onClick={() => printMsg("To be implemented...")}>View</Button></S.SBT4></ListGroup.Item>
+                                        {/* <ListGroup.Item><S.SBT4><Button className='b' variant="outline-secondary" onClick={() => printMsg("To be implemented...")}>View</Button></S.SBT4></ListGroup.Item> */}
                                     </ListGroup>
                                 </Stack>
                             </Row>
                             <ColoredHR></ColoredHR>
                             {/* <Row>
-=======
-                        <br/><br/>
-                        <Row xs="3">
-                            <Col><ListGroup horizontal>
-                                <ListGroup.Item><S.Account1>Account</S.Account1></ListGroup.Item>
-                                <ListGroup.Item><S.Account2>{active ? account : "-"}</S.Account2></ListGroup.Item>
-                            </ListGroup></Col>
-                            <Col><Button className='b' variant='outline-warning' size='lg' onClick={() => handleShow_Mint('md-down')}>Mint STT</Button></Col>
-                            <Col><Button className='b' variant='outline-warning' size='lg'onClick={handdleConnect}>{active ? "Disconnect" : "Connect"}</Button></Col>
-                        </Row>
-                        <br/><br/><br/><br/>
-                        <ColoredHR></ColoredHR>
-                        {/* SBT */}
-                        <Row>
-                            <Col>
-                                <Row>
-                                    <Col><Card.Title className='c' align='left'>My SBT</Card.Title></Col><Col></Col><Col></Col>
-                                </Row>
-                                <Row><p className='c' align='left'>This part can be added dynamically, later.</p>
-                                    <Stack gap={3}>
-                                        <ListGroup horizontal className='c'>
-                                            <ListGroup.Item><S.SBT1><Badge bg="secondary">10</Badge></S.SBT1></ListGroup.Item>
-                                            <ListGroup.Item><S.SBT2>blah blah no example</S.SBT2></ListGroup.Item>
-                                            <ListGroup.Item><S.SBT3><Button className='b' variant="outline-secondary" onClick={() => printMsg("To be implemented...")}>View</Button></S.SBT3></ListGroup.Item>
-                                        </ListGroup>
-                                        <ListGroup horizontal='sm' className='d'>
-                                            <ListGroup.Item><S.SBT1><Badge bg="secondary">30</Badge></S.SBT1></ListGroup.Item>
-                                            <ListGroup.Item><S.SBT2>blah blah 'SM' example</S.SBT2></ListGroup.Item>
-                                            <ListGroup.Item><S.SBT3><Button className='b' variant="outline-secondary" onClick={() => handleShow('md-down')}>View</Button></S.SBT3></ListGroup.Item>
-                                        </ListGroup>
-                                        <ListGroup horizontal='md' className='d'>
-                                            <ListGroup.Item><S.SBT1><Badge bg="secondary">50</Badge></S.SBT1></ListGroup.Item>
-                                            <ListGroup.Item><S.SBT2>blah blah 'MD' example</S.SBT2></ListGroup.Item>
-                                            <ListGroup.Item><S.SBT3><Button className='b' variant="outline-secondary" onClick={() => printMsg("To be implemented...")}>View</Button></S.SBT3></ListGroup.Item>
-                                        </ListGroup>
-                                        <br/><br/><br/>
-                                    </Stack>
-                                </Row>
-                                <ColoredHR></ColoredHR>
-                                    {/* <Row>
->>>>>>> f0bea5b851309ccc05cc90c1f37a73d20ebcf588
                                         <Col><Card.Title>My STT</Card.Title></Col><Col></Col><Col></Col>
                                     </Row>
                                     <Row>This part also can be added dynamically, later.
--- dapp/src/components/Ui/index.js
+++ dapp/src/components/Ui/index.js
@@ -13,40 +13,6 @@ import ClientPage from '../../pages/ClientPage';
 import MainPage from '../../pages/MainPage';
 import TestPage from '../../pages/TestPage';
 
-<<<<<<< HEAD
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
-// import Tab from 'react-bootstrap/Tab';
-
-import * as S from './style';
-import './index.css';
-
-const Ui = () => {
-    return (
-        <Tabs
-            defaultActiveKey="main"
-            id="uncontrolled-tab-example"
-            className="mb-3"
-        >
-            <S.Container>            </S.Container>
-            <Tab eventKey="main" title="Main">
-                <MainPage></MainPage>
-            </Tab>
-
-            <Tab eventKey="host" title="Host">
-                <HostPage></HostPage>
-            </Tab>
-            <Tab eventKey="client" title="Client">
-                <ClientPage></ClientPage>
-            </Tab>
-            <Tab eventKey="text" title="Test">
-                <TestPage></TestPage>
-            </Tab>
-        </Tabs>
-=======
-
-
 import './index.css'
 
 const Ui = () => {
@@ -73,7 +39,6 @@ const Ui = () => {
                 </Tab>
             </Tabs>
         </>
->>>>>>> f0bea5b851309ccc05cc90c1f37a73d20ebcf588
     )
 }
 export default Ui;
